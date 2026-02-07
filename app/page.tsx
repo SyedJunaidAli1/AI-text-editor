1445b8a1eed68760544e46217f95f38c5a8fc2ea
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-07 15:58:01 +0530

    shadcn initialized

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import { createClient } from "@/lib/supabase/client";
 export default async function Home() {
   const supabase = createClient();
 
-  const { data, error } = await supabase.from("Todos").select("*");
+  // const { data, error } = await supabase.from("Todos").select("*");
 
-  if (error) {
-    console.log(error);
-  }
+  // if (error) {
+  //   console.log(error);
+  // }
 
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-2 sm:items-start">
         <h1 className="text-4xl font-bold">Welcome to AI Text Editor</h1>
-        <p>{JSON.stringify(data, null, 2)}</p>
+        {/*<p>{JSON.stringify(data, null, 2)}</p>*/}
       </main>
     </div>
   );
