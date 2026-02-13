8fa1332e8df285e35f10fb3107dffa43027c370d
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-13 16:03:15 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import { createClient } from "@/lib/supabase/client";
 import { AnimatedThemeToggler } from "./components/AnimatedThemeToggler";
 
 export default async function Home() {
-  const supabase = createClient();
-
-  // const { data, error } = await supabase.from("Todos").select("*");
-
-  // if (error) {
-  //   console.log(error);
-  // }
-
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-2 sm:items-start">
+      <main className="flex gap-4 min-h-screen w-full max-w-3xl items-center py-2 sm:items-start">
         <h1 className="text-4xl font-bold">Welcome to AI Text Editor</h1>
         <AnimatedThemeToggler />
-        {/*<p>{JSON.stringify(data, null, 2)}</p>*/}
       </main>
     </div>
   );
