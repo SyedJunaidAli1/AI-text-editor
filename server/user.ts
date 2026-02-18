a0d2a1af9f369520c40bbf92bdae8aa8b4c5acad
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-18 17:30:14 +0530

    getUser fnc in server.ts

--- server/user.ts
+++ server/user.ts
@@ -51,3 +51,14 @@ export async function signinUser({ email, password }: SignInParams) {
     throw new Error("SignIn failed");
   }
 }
+
+export async function getUser() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    console.error("Auth error", error.message);
+    return null
+  }
+  return data.user;
+}
