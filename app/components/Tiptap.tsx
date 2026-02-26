673d6bbee74344cf88dcb9bbbd3d114f174de0a7
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-26 23:49:20 +0530

    some minor changes

--- app/components/Tiptap.tsx
+++ app/components/Tiptap.tsx
@@ -1,17 +1,27 @@
-'use client'
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent } from "@tiptap/react";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    // element: document.querySelector('.element'),
+    // onUpdate({ editor }) {
+    //    const json = editor.getJSON();
+    //    console.log("Document JSON:", json);
+    //  },
+    extensions: [Document, Paragraph, Text],
+    content: "<p>Hello World! 🌎️</p>",
+    autofocus: true,
+    editable: true,
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
-  })
+    injectCSS: false,
+  });
 
-  return <EditorContent editor={editor} />
-}
+  return <EditorContent editor={editor} />;
+};
 
-export default Tiptap
+export default Tiptap;
