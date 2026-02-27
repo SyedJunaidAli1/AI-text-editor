a87385ac4951330c573d8eb81028cda61b8272cf
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-27 15:54:22 +0530

    text editor btn add

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -4,8 +4,8 @@ const app = () => {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="min-h-screen w-full max-w-3xl items-center py-2 sm:items-start">
-        <h1 className="text-4xl font-bold">AI Text Editor welcome to app</h1>
-        <Tiptap />
+        <h1 className="text-4xl font-bold mb-10">AI Text Editor welcome to app</h1>
+        <Tiptap  />
       </main>
     </div>
   );
--- app/components/Tiptap.tsx
+++ app/components/Tiptap.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
-import StarterKit from '@tiptap/starter-kit';
-
+import {
+  useEditor,
+  EditorContent,
+  useEditorState,
+  Editor,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { Button } from "@/components/ui/button";
 
 const Tiptap = () => {
   const editor = useEditor({
@@ -13,16 +15,58 @@ const Tiptap = () => {
     //   const json = editor.getJSON();
     //   console.log("Document JSON:", json);
     // },
-    extensions: [StarterKit,Document, Paragraph, Text],
+    extensions: [StarterKit],
     content: "<p>Hello World! 🌎️</p>",
-    autofocus: true,
-    editable: true,
+    // autofocus: true,
+    // editable: true,
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
-    injectCSS: false,
+    // injectCSS: false,
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <>
+      {editor && <Toolbar editor={editor} />}
+      <EditorContent editor={editor} />
+    </>
+  );
+};
+
+const Toolbar = ({ editor }: { editor: Editor }) => {
+  const editorState = useEditorState({
+    editor,
+    selector: (ctx) => {
+      return {
+        isBold: ctx.editor.isActive("bold") ?? false,
+        isItalic: ctx.editor.isActive("italic") ?? false,
+        isStrike: ctx.editor.isActive("strike") ?? false,
+      };
+    },
+  });
+  return (
+    <>
+      <Button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={editorState.isBold ? "is-active" : ""}
+      >
+        Bold
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+  
+        className={editorState.isItalic ? "is-active" : ""}
+      >
+        Italic
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+
+        className={editorState.isStrike ? "is-active" : ""}
+      >
+        Strike
+      </Button>
+    </>
+  );
 };
 
 export default Tiptap;
