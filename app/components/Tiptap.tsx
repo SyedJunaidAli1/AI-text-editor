74d20e8ee4d6ce17e3412df3a676a223e5279ca8
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-02 15:00:45 +0530

    Code and Codeblock style is done

--- app/components/Tiptap.tsx
+++ app/components/Tiptap.tsx
@@ -48,7 +48,7 @@ const Tiptap = () => {
     content: "<p>Hello World! 🌎️</p>",
     editorProps: {
       attributes: {
-        class: `prose dark:prose-invert prose-blockquote:border-l-4 prose-blockquote:border-primary prose-blockquote: pl-4 prose-code:bg-indigo-500/10 prose-code:px-1.5 prose-code:py-0.5 prose-code:rounded prose-pre:bg-gray-900 prose-pre:text-gray-100 prose-pre:rounded-lg prose-pre:p-4 prose-code:before:content-noneprose-code:after:content-none focus:outline-none max-w-none`,
+        class: `prose dark:prose-invert prose-blockquote:border-l-4 prose-blockquote:border-primary prose-blockquote:pl-4 prose-code:px-1.5 prose-code:bg-secondary prose-code:text-foreground prose-pre:bg-secondary prose-code:font-normal prose-code:text-sm prose-code:px-1.5 prose-code:py-0.5 prose-code:rounded prose-code:before:content-none prose-code:after:content-none focus:outline-none max-w-none`,
       },
     },
     // autofocus: true,
