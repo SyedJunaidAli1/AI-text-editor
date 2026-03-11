793ecf1113273fd50b44860aff14bbeb3de10a8a
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-11 15:42:54 +0530

    dependencies update

--- app/proxy.ts
+++ app/proxy.ts
@@ -1,50 +1,70 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { createServerClient } from "@supabase/ssr";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { createServerClient } from "@supabase/ssr";
 
-const authPaths = ["/signin", "/signup", "/forgot-password", "/reset-password"];
-const protectedPaths = ["/app"];
+// const authPaths = ["/signin", "/signup", "/forgot-password", "/reset-password"];
+// const protectedPaths = ["/app", "/test"];
 
-export async function proxy(request: NextRequest) {
-  const response = NextResponse.next();
+// export async function middleware(request: NextRequest) {
+//   const response = NextResponse.next();
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options),
-          );
-        },
-      },
-    },
-  );
+//   const supabase = createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+//     {
+//       cookies: {
+//         getAll() {
+//           return request.cookies.getAll();
+//         },
+//         setAll(cookiesToSet) {
+//           cookiesToSet.forEach(({ name, value, options }) =>
+//             response.cookies.set(name, value, options),
+//           );
+//         },
+//       },
+//     },
+//   );
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser();
 
-  const { pathname } = request.nextUrl;
+//   console.log(user);
 
-  // 🚫 Not logged in → accessing protected routes
-  if (!user && protectedPaths.some((path) => pathname.startsWith(path))) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+//   const { pathname } = request.nextUrl;
 
-  // ✅ Logged in → accessing auth pages
-  if (user && authPaths.some((path) => pathname.startsWith(path))) {
-    return NextResponse.redirect(new URL("/app", request.url));
-  }
+//   // 🚫 Not logged in → accessing protected routes
+//   if (!user && protectedPaths.some((path) => pathname.startsWith(path))) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
 
-  return response;
+//   // ✅ Logged in → accessing auth pages
+//   if (user && authPaths.some((path) => pathname.startsWith(path))) {
+//     return NextResponse.redirect(new URL("/app", request.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//   ],
+// };
+
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
 }
-
+ 
+console.log("middleware")
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+ 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-  ],
-};
+  matcher: '/about/:path*',
+}
