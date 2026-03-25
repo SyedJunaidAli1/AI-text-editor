657158984e484b4a0e00df270826168692fa9cae
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-25 13:08:05 +0530

    button added for main app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { AnimatedThemeToggler } from "./components/AnimatedThemeToggler";
+import Link from "next/link";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="flex gap-4 min-h-screen w-full max-w-3xl items-center py-2 sm:items-start">
         <h1 className="text-4xl font-bold">Welcome to AI Text Editor</h1>
         <AnimatedThemeToggler />
+        <div>
+          <Link href="/app">
+            <Button>Get started</Button>
+          </Link>
+        </div>
       </main>
     </div>
   );
