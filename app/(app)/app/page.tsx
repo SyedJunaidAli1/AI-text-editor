804186b54383b2b5b326ba78e910a52fc1ad20e7
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-30 13:49:47 +0530

    made (otherroute) for least imp pages and implemented id in app route

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -1,23 +1,28 @@
 import Tiptap from "@/app/components/Tiptap";
 
-const app = () => {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { docId?: string };
+}) {
+  const docId = searchParams.docId;
+
   return (
     <div className="flex min-h-screen w-full py-4 px-2">
-      {/* EDITOR (LEFT) */}
+      {/* EDITOR */}
       <main className="flex-1">
         <h1 className="text-4xl font-bold mb-10">
           AI Text Editor welcome to app
         </h1>
-        <Tiptap />
+
+        {/* 👇 pass docId */}
+        <Tiptap docId={docId} />
       </main>
 
-      {/* AI PANEL (RIGHT) */}
+      {/* AI PANEL */}
       <div className="w-[30%] border border-red-600 h-screen px-2">
         <h2 className="text-2xl font-bold mb-4">AI Assistant</h2>
-        {/* AI chat UI goes here */}
       </div>
     </div>
   );
-};
-
-export default app;
+}
