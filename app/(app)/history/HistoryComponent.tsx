2e6c86f562200d03782ab75aa0a958719d8b80ea
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-04-06 13:26:18 +0530

    history component uiux wip

--- app/(app)/history/HistoryComponent.tsx
+++ app/(app)/history/HistoryComponent.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { documentsQuery } from "@/lib/tanstack-queries/document";
+import { useRouter } from "next/navigation";
+
 const HistoryComponent = () => {
-  
-  
+  const { data: docs, isLoading } = useQuery(documentsQuery.all());
+  const router = useRouter();
+
+  if (isLoading) return <p>Loading...</p>;
+
   return (
-    <>
-      <h1>History page</h1>
-    </>
+    <div className="px-2 py-6 space-y-2">
+      {docs?.map((doc) => (
+        <div
+          key={doc.id}
+          onClick={() => router.push(`/app?docId=${doc.id}`)}
+          className="px-4 py-8 rounded-lg border cursor-pointer hover:bg-muted transition"
+        >
+          <p className="font-medium">{doc.title || "Untitled"}</p>
+          <p className="text-xs text-muted-foreground">
+            {new Date(doc.updated_at).toLocaleString()}
+          </p>
+        </div>
+      ))}
+    </div>
   );
 };
 
