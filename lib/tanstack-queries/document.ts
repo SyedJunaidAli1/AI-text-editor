ee6c06acfeea69f662dbc87f650ea6e3b2f8da25
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-04-19 13:43:22 +0530

    tanstackquery new query added byId

--- lib/tanstack-queries/document.ts
+++ lib/tanstack-queries/document.ts
@@ -1,8 +1,13 @@
-import { getAllDocuments } from "@/server/document";
+import { getAllDocuments, getDocumentsById } from "@/server/document";
 
 export const documentsQuery = {
   all: () => ({
     queryKey: ["documents"],
     queryFn: getAllDocuments,
+  }),
+
+  byId: (id: string) => ({
+    queryKey: ["documents", id],
+    queryFn: () => getDocumentsById(id),
   }),
 };
