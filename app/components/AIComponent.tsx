ebe04a10e26c09ebc5e38d9566f8af4ceff6ecd2
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-05-06 16:12:00 +0530

    feat: shadcn inputgroup add

---
+++ app/components/AIComponent.tsx
@@ -0,0 +1,26 @@
+const AIComponent = () => {
+  return (
+    <>
+      <div className="flex w-full max-w-md flex-col gap-4">
+        <InputGroup >
+          <InputGroupInput placeholder="Send a message..." disabled />
+          <InputGroupAddon align="inline-end">
+            <Spinner />
+          </InputGroupAddon>
+        </InputGroup>
+        <InputGroup>
+          <InputGroupTextarea placeholder="Send a message..." disabled />
+          <InputGroupAddon align="block-end">
+            <Spinner /> Validating...
+            <InputGroupButton className="ml-auto" variant="default">
+              <ArrowUpIcon />
+              <span className="sr-only">Send</span>
+            </InputGroupButton>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+    </>
+  );
+};
+
+export default AIComponent;
