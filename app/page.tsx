3063d7f0a33c1cf71549db3b6b203dd12ad7fe0e
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-06-19 17:18:12 +0530

    feat: navbar for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@
 import { Button } from "@/components/ui/button";
 import { AnimatedThemeToggler } from "./components/AnimatedThemeToggler";
 import Link from "next/link";
+import Nav from "@/app/components/Nav";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex gap-4 min-h-screen w-full max-w-3xl items-center py-2 sm:items-start">
-        <h1 className="text-4xl font-bold">Welcome to AI Text Editor</h1>
+    <div className="flex min-h-screen items-start justify-center font-sans">
+      <main className="flex gap-4  w-full justify-center py-2">
+        <Nav />
+        {/*<h1 className="text-4xl font-bold">Welcome to AI Text Editor</h1>
         <AnimatedThemeToggler />
         <div>
           <Link href="/app">
             <Button>Get started</Button>
           </Link>
-        </div>
+        </div>*/}
       </main>
     </div>
   );
