0027cba0b2f6ec18ce9a0f75d183164f7af4d467
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-06-25 17:11:57 +0530

    fix: tailwind syntax fix

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,11 +4,11 @@ import { motion } from "motion/react";
 
 const HeroSection = () => {
   return (
-    <section className="bg-[url(/hero-image-bg.png)] relative h-[800] w-full rounded-lg mt-12 pt-16 overflow-hidden">
+    <section className="bg-[url(/hero-image-bg.png)] relative h-[800px] w-full rounded-lg mt-12 pt-16 overflow-hidden">
       <motion.div
         drag
         dragMomentum={false}
-        className="absolute inset-x-0 mx-auto w-5xl h-[700] bg-background rounded-lg border shadow-2xl"
+        className="absolute inset-x-0 mx-auto w-5xl h-[700px] bg-background rounded-lg border shadow-2xl"
       ></motion.div>
     </section>
   );
