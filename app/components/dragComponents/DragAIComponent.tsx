6b5abbfa2dd970123f7b067603aca359ca98b81a
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-07-03 17:23:43 +0530

    feat: all most done with drag ai component

--- app/components/dragComponents/DragAIComponent.tsx
+++ app/components/dragComponents/DragAIComponent.tsx
@@ -8,7 +8,10 @@ import { SparkleIcon, ArrowUpIcon } from "@phosphor-icons/react";
 
 const DragAIComponent = ({ onclose }: { onclose: () => void }) => {
   return (
-    <div className="bg-background w-85 h-screen border-background border-l-2">
+    <div
+      className={`bg-background h-screen border-background border-l-2 transition-all duration-500 ease-in-out
+        ${onclose ? "w-0" : "w-85"}`}
+    >
       <section className="flex flex-col px-2 py-4 gap-2 border-b">
         <div className="flex items-center gap-2">
           <SparkleIcon size={32} className="text-primary" />
@@ -21,7 +24,15 @@ const DragAIComponent = ({ onclose }: { onclose: () => void }) => {
       </section>
 
       {/*chat section*/}
-      <section className="flex flex-col gap-2 h-110 overflow-y-scroll"></section>
+      <section className="flex flex-col gap-2 h-110 px-2 overflow-y-scroll mt-3">
+        <p className="rounded-xl py-2 px-3 text-sm max-w-[80%] bg-primary text-primary-foreground ml-auto">
+          hey can you check the document
+        </p>
+
+        <p className="rounded-xl py-2 px-3 text-sm max-w-[80%] bg-muted text-primary-foreground mr-auto">
+          i checked he document there are few incoming meetings
+        </p>
+      </section>
 
       {/*input section*/}
       <div className="border-t p-4">
