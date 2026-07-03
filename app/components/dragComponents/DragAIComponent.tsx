a16e57eae26183b222b7815657c9d0811a351f4b
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-07-03 15:40:41 +0530

    feat: drag ai component in work in progress

--- app/components/dragComponents/DragAIComponent.tsx
+++ app/components/dragComponents/DragAIComponent.tsx
@@ -1,7 +1,18 @@
-const DragAIComponent = () => {
+import { SparkleIcon } from "@phosphor-icons/react";
+
+const DragAIComponent = ({ onclose }: { onclose: () => void }) => {
   return (
-    <div className="bg-background w-80 h-screen border-background border-l-2">
-      
+    <div className="bg-background w-85 h-screen border-background border-l-2">
+      <section className="flex flex-col px-2 py-4 gap-2 border-b">
+        <div className="flex items-center gap-2">
+          <SparkleIcon size={32} className="text-primary" />
+          <p>AI Assistant</p>
+        </div>
+
+        <p className="text-xs text-muted-foreground">
+          Ask AI to write, summarize, or improve text
+        </p>
+      </section>
     </div>
   );
 };
