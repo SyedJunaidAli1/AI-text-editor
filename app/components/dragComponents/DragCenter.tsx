06aa9a6f234da441eb17d5216e4acc10d6bc5161
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-07-08 16:10:40 +0530

    feat: text editor nav

--- app/components/dragComponents/DragCenter.tsx
+++ app/components/dragComponents/DragCenter.tsx
@@ -2,7 +2,26 @@ import { Input } from "@/components/ui/input";
 import DragAIComponent from "./DragAIComponent";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { RobotIcon } from "@phosphor-icons/react";
+import {
+  ArrowUUpLeftIcon,
+  ArrowUUpRightIcon,
+  CodeBlockIcon,
+  CodeIcon,
+  RobotIcon,
+  TextAlignCenterIcon,
+  TextAlignJustifyIcon,
+  TextAlignLeftIcon,
+  TextAlignRightIcon,
+  TextBIcon,
+  TextIndentIcon,
+  TextItalicIcon,
+  TextStrikethroughIcon,
+  TextSubscriptIcon,
+  TextSuperscriptIcon,
+  TextUnderlineIcon,
+} from "@phosphor-icons/react";
+import { Separator } from "@/components/ui/separator";
+import { Toggle } from "@/components/ui/toggle";
 
 const DragCenter = () => {
   const [show, setShow] = useState(true);
@@ -25,9 +44,60 @@ const DragCenter = () => {
           placeholder="Enter Description..."
           className="text-muted-foreground px-0 focus-visible:ring-0 border-none w-full max-w-xl bg-transparent"
         />
+        <div className="w-full flex flex-wrap items-center border-b gap-1">
+          <Button variant="ghost" className="px-2">
+            <ArrowUUpLeftIcon size={24} />
+          </Button>
+          <Button variant="ghost" className="px-2">
+            <ArrowUUpRightIcon size={24} />
+          </Button>
+
+          <Toggle aria-label="Blockquote">
+            <TextIndentIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Code Block">
+            <CodeBlockIcon size={24} />
+          </Toggle>
+
+          <Toggle aria-label="Bold">
+            <TextBIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Italic">
+            <TextItalicIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Strike">
+            <TextStrikethroughIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Code">
+            <CodeIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Underline">
+            <TextUnderlineIcon size={24} />
+          </Toggle>
+
+          <Toggle aria-label="Superscript">
+            <TextSuperscriptIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Subscript">
+            <TextSubscriptIcon size={24} />
+          </Toggle>
+
+          <Toggle aria-label="Align Left">
+            <TextAlignLeftIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Align Center">
+            <TextAlignCenterIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Align Right">
+            <TextAlignRightIcon size={24} />
+          </Toggle>
+          <Toggle aria-label="Align Justify">
+            <TextAlignJustifyIcon size={24} />
+          </Toggle>
+        </div>
       </section>
       <div
-        className={`transition-all duration-300
+        className={`transition-all ease-in-out duration-300
         ${show ? "w-[42%]" : "w-0"}
       `}
       >
