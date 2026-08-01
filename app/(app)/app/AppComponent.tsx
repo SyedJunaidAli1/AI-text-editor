49ab518b792631ac020e617ea5c9bfdd4acf7e75
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-08-01 15:25:46 +0530

    feat: minor clean up

--- app/(app)/app/AppComponent.tsx
+++ app/(app)/app/AppComponent.tsx
@@ -77,7 +77,6 @@ export default function AppComponent({ docId }: { docId?: string }) {
         className={`
           fixed inset-0 z-50 bg-background md:hidden
           transition-transform duration-300 ease-in-out
-
           ${isAIOpen ? "translate-x-0" : "translate-x-full pointer-events-none"}
         `}
       >
